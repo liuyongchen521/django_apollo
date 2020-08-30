7e4b5c9d07ad65caf9f6f97dc4f503d185f97712
liuyongchen <liuyongchen_job@163.com>
2020-08-30 23:14:12 +0800

    实现了a级别的安全路由

--- apollo_app/urls.py
+++ apollo_app/urls.py
@@ -3,4 +3,5 @@ from apollo_app import views
 
 urlpatterns = [
     path('login/', views.login),
+    path('safe_a/', views.safe_a),#配置访问a等级安全的路由配置
 ]
--- apollo_app/views.py
+++ apollo_app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def login(request):
     return HttpResponse("login")
+def safe_a(request):
+    return HttpResponse("实现了A等级的安全级别")
